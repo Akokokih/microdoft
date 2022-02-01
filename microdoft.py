31484ef62f55cedadd903cc954f86c823e299016
Akokokih <akokokih.games@gmail.com>
2022-02-02 00:18:12 +0100

    few corrections

--- microdoft.py
+++ microdoft.py
@@ -1,5 +1,6 @@
 from socket import *
 from requests.packages.urllib3.contrib import pyopenssl as reqs
+import csv
 
 def get_possible_domain():
     text = "microsoft"
@@ -7,7 +8,7 @@ def get_possible_domain():
     rank = 0
     ascii_values = []
     possible_mot = []
-    for character in range(len(text)-1):
+    for character in range(len(text)):
         try:
             textchanged = textchanged[1:]
         except:
@@ -23,12 +24,11 @@ def get_possible_domain():
             possible_mot.append(result)
         rank = rank + 1
     list_domain = [s + ".com" for s in possible_mot]
-    print(list_domain)
     return(list_domain)
 
 def https_cert_subject_alt_names(host, port):
     """Read subject domains in https cert from remote server"""
-    print(host,port)
+    print("Trying host : ",host,port)
     setdefaulttimeout(3)
     x509 = reqs.OpenSSL.crypto.load_certificate(
         reqs.OpenSSL.crypto.FILETYPE_PEM,
@@ -36,20 +36,25 @@ def https_cert_subject_alt_names(host, port):
     )
     return reqs.get_subj_alt_name(x509)
     
+def export_file(file_name, json_dict):
+    myCsv = csv.writer(open(file_name, 'w'))
+    for key in json_dict:
+        myCsv.writerow([key, json_dict[key]])
+    
 if __name__ == '__main__':
     result={}
+    last_result={}
     for elem in get_possible_domain():
         try:
             domains = https_cert_subject_alt_names(elem, 443)
             if isinstance(domains, list):
                 result[elem] = domains
-                print(type(domains), domains)
         except:
             pass
     
     for elem in result:
-        if len(dict_result[elem]):
-            for element in range(len(dict_result[elem])):
-                if "microsoft" in dict_result[elem][element][1]:
-                    last_result[elem] = dict_result[elem]
-    print(last_result)
+        if len(result[elem]):
+            for element in range(len(result[elem])):
+                if "microsoft" in result[elem][element][1]:
+                    last_result[elem] = result[elem]
+    export_file('Item.csv', last_result)
